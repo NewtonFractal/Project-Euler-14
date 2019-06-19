6e302e9fddaaf8a9b22b5398055e90b55bc24386
Newton_Fractal <51488709+NewtonFractal@users.noreply.github.com>
2019-06-19 12:35:01 +0200

    Update Project Euler #14.py

--- Project Euler #14.py
+++ Project Euler #14.py
@@ -1,25 +1,21 @@
 import time
 start = time.time()
-def Longest_Collatz_sequence(x,chain,record):
-    for x in range(x, 1000000):
-        if record == 524:
-            print(x-1)
-            break
-        Loop = True
-        while  Loop == True:
+chains = []
+
+def Longest_Collatz_sequence(chain):
+    for x in range(3,1000000,2):
+        chains.append(chain)
+        chain = 0
+        while x > 1:
             if x % 2 == 0:
                 x /= 2
                 chain += 1
             else:
-                x = 3 * x + 1
+                x = 3*x +1
                 chain += 1
-            if x == 1:
-                Loop = False
-                if chain > record:
-                    record = chain
-                chain = 0
-    print(record)
 
-Longest_Collatz_sequence(2,0,0)
+
+Longest_Collatz_sequence(0)
 end = time.time()
 print(end - start)
+print(max(chains))
